6dd746199801c4ac41a58df086cf3904b9d0da12
zhoushaoyou <zhoushaoyou@skyworth.com>
2018-09-18 08:39:49 +0800

    Initial Commit

---
+++ SafaTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafaTimer
+//
+//  Created by zhouzhou on 2018/9/18.
+//  Copyright © 2018年 zhouzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SafaTimer/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SafaTimer
+//
+//  Created by zhouzhou on 2018/9/18.
+//  Copyright © 2018年 zhouzhou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
